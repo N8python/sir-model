cd46ed7a8a5c09d837e0c62e1a81cb8bb388279a
n8programs <43304488+N8python@users.noreply.github.com>
2020-06-30 11:50:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let doingSimulation = false;
+let updateInterval;
 let model = { destroy() {} }
 document.getElementById("start").onclick = () => {
     if (doingSimulation) {
@@ -9,6 +10,7 @@ document.getElementById("start").onclick = () => {
     const pop = document.getElementById("size").value;
     let susceptible;
     let infected;
+    let removed = 0;
     const i = document.getElementById("I").value;
     if (!i) {
         susceptible = (pop - 1) / pop;
@@ -17,8 +19,11 @@ document.getElementById("start").onclick = () => {
         susceptible = 1 - i;
         infected = +i;
     }
-    const l = document.getElementById("l").value;
-    const y = document.getElementById("y").value;
+    const l = +document.getElementById("l").value;
+    const y = +document.getElementById("y").value;
+    const u = +document.getElementById("u").value;
+    const v = +document.getElementById("v").value;
+    const a = +document.getElementById("a").value;
     let timestep = 0;
     model = new Chart(document.getElementById("model"), {
         type: "line",
@@ -35,7 +40,7 @@ document.getElementById("start").onclick = () => {
                 fill: false,
                 borderColor: "#c45850"
             }, {
-                data: [(1 - susceptible - infected) * pop],
+                data: [removed * pop],
                 label: "Removed",
                 fill: false,
                 borderColor: "#999999"
@@ -48,13 +53,21 @@ document.getElementById("start").onclick = () => {
             }
         }
     });
-    let updateInterval = setInterval(() => {
+    updateInterval = setInterval(() => {
         timestep++;
         const s = susceptible;
         const i = infected;
+        const r = removed;
         susceptible -= l * s * i;
+        susceptible += v;
+        susceptible += a * r;
+        susceptible -= u * s;
         infected += l * s * i;
         infected -= y * i;
+        infected -= u * i;
+        removed += y * i;
+        removed -= u * r;
+        removed -= a * r;
         model.data.labels.push("t" + timestep);
         model.data.datasets.forEach(dataset => {
             switch (dataset.label) {
@@ -65,7 +78,7 @@ document.getElementById("start").onclick = () => {
                     dataset.data.push(Math.round(infected * pop));
                     break;
                 case "Removed":
-                    dataset.data.push(Math.round((1 - susceptible - infected) * pop));
+                    dataset.data.push(Math.round(removed * pop));
                     break;
             }
         })
@@ -76,4 +89,8 @@ document.getElementById("start").onclick = () => {
         }
 
     }, document.getElementById("speed").value * 1000);
+}
+document.getElementById("end").onclick = () => {
+    doingSimulation = false;
+    clearInterval(updateInterval);
 }
